f96191604feeebcca6abbf143c62f2da6408e8b6
Honour <nosakhare073@gmail.com>
2023-09-21 21:25:48 +0100

    font, svgs, images

--- assets/svgs/index.ts
+++ assets/svgs/index.ts
@@ -1,3 +1,55 @@
+// background
 export { default as FlareOne } from './flare/flare-one.svg'
 export { default as FlareTwo } from './flare/flare-two.svg'
+export { default as FlareFour } from './flare/flare-four.svg'
+export { default as FlareFive } from './flare/flare-five.svg'
+export { default as FlareSix } from './flare/flare-six.svg'
+export { default as FlareLast } from './flare/flare-last.svg'
+export { default as Ellipse } from './ellipse.svg'
+
+// random
 export { default as TitleSvg } from './title.svg'
+export { default as SubTitleSvg } from './sub-title.svg'
+export { default as CountdownSvg } from './countdown.svg'
+export { default as QuestionMark } from './question-mark.svg'
+export { default as MobileMenu } from './mobile-menu.svg'
+export { default as GreenCheck } from './green-check.svg'
+
+// main
+export { default as TheBigIdea } from './the-big-idea.svg'
+export { default as ChairGirl } from './chair-girl.svg'
+export { default as StandingMan_Woman } from './standing-man-woman.svg'
+export { default as CloudMan } from './cloud-man.svg'
+export { default as Championship } from './championship.svg'
+export { default as PadlockMan } from './padlock-man.svg'
+export { default as Security } from './security.svg'
+export { default as ThumbsUpMan } from './thumbs-up-man.svg'
+export { default as CongratulationsMan } from './congratulations-man.svg'
+
+//medals
+export { default as Silver } from './medals/silver.svg'
+export { default as Gold } from './medals/gold.svg'
+export { default as Bronze } from './medals/bronze.svg'
+
+//star
+export { default as StarOneScreenOne } from './stars/screen-one/one.svg'
+export { default as StarTwoScreenOne } from './stars/screen-one/two.svg'
+export { default as StarOneScreenTwo } from './stars/screen-two/one.svg'
+export { default as StarTwoScreenTwo } from './stars/screen-two/two.svg'
+export { default as CurledArrowScreenTwo } from './stars/screen-two/curled-arrow.svg'
+
+// sponsors
+export { default as LibertyAssured } from './sponsors/liberty-assured.svg'
+export { default as LibertyPay } from './sponsors/liberty-pay.svg'
+export { default as Whispersms } from './sponsors/whispersms.svg'
+export { default as Winwise } from './sponsors/winwise.svg'
+
+//socials
+export { default as Facebook } from './socials/facebook.svg'
+export { default as Twitter } from './socials/twitter.svg'
+export { default as Linkedin } from './socials/linkedin.svg'
+export { default as Instagram } from './socials/instagram.svg'
+
+//contact
+export { default as Phone } from './socials/contact/phone.svg'
+export { default as Location } from './socials/contact/location.svg'
